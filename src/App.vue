1bd731492ed3f2074afa6bf4292bce7b7d9671be
nokazn <baleariclemon@gmail.com>
2019-11-26 02:07:46 +0900

    boilerplate

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'Hello World'
+    };
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  font-size: 18px;
+  font-family: sans-serif;
+}
+</style>
